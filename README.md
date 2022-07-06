78adbda3644ea9c1c67d85c7122094d3bd368691
Eradev <admin@eradev.com>
2022-07-06 10:45:50 -0400

    DataDumper 1.0.0 RandomRandomnizer 1.0.2

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Mods for Monster Sanctuary
 
+## Data Dumper
+
+Dumps some data into .json files.
+
 ## Random Randomizer
 
 Randomize random encounters and chests' content in Randomizer mode.
--- RandomRandomizer/RandomRandomizerPlugin.cs
+++ RandomRandomizer/RandomRandomizerPlugin.cs
@@ -14,31 +14,36 @@ namespace eradev.monstersanctuary.RandomRandomizer
         private static readonly System.Random Rand = new();
         private static ManualLogSource _log;
 
-        private static bool _isRandomizerMode;
         private static List<GameObject> _possibleItemsList;
 
         private const bool RandomizedMonstersEnabledDefault = true;
+        private const string MonstersBlacklistDefault = "228,317,348,361,1879"; // Spectral (228,317,348,361), Bard (1879)
 
         private static ConfigEntry<bool> _randomizeMonstersEnabled;
+        private static ConfigEntry<string> _monstersBlacklist;
 
         private const bool RandomizedChestsEnabledDefault = true;
         private const float GoldChanceDefault = 0.05f;
         private const int MinGoldDefault = 5;
         private const int MaxGoldDefault = 50;
+        private const string ItemsBlacklistDefault = "1792,1793,1794,1795,1796,1797"; // Eternity Flame (1792~1797)
 
         private static ConfigEntry<bool> _randomizeChestsEnabled;
         private static ConfigEntry<float> _goldChance;
         private static ConfigEntry<int> _minGold;
         private static ConfigEntry<int> _maxGold;
+        private static ConfigEntry<string> _itemsBlacklist;
 
         private void Awake()
         {
             _randomizeMonstersEnabled = Config.Bind("Randomized Monsters", "Enabled", RandomizedMonstersEnabledDefault, "Randomize monsters");
+            _monstersBlacklist = Config.Bind("Randomized Monsters", "Blacklist", MonstersBlacklistDefault, "Blacklisted monsters ID");
 
             _randomizeChestsEnabled = Config.Bind("Randomized Chests", "Enabled", RandomizedChestsEnabledDefault, "Randomize chests");
             _goldChance = Config.Bind("Randomized Chests", "Chance for gold", GoldChanceDefault, "Chance to get gold in chests (0.0 = never, 1.0 = always)");
             _minGold = Config.Bind("Randomized Chests", "Minimum gold", MinGoldDefault, "Minimum value of gold in chests (x100, must be > 0)");
             _maxGold = Config.Bind("Randomized Chests", "Maximum gold", MaxGoldDefault, "Minimum value of gold in chests (x100, must be > 0)");
+            _itemsBlacklist = Config.Bind("Randomized Chests", "Blacklist", ItemsBlacklistDefault, "Blacklisted items ID");
 
             // Ensure minGold is always higher than 0.
             if (_minGold.Value == 0)
@@ -74,6 +79,7 @@ namespace eradev.monstersanctuary.RandomRandomizer
         [HarmonyPatch(typeof(MonsterEncounter), "Start")]
         private class MonsterEncounterStartPatch
         {
+            // ReSharper disable once InconsistentNaming
             private static void Prefix(ref MonsterEncounter __instance)
             {
                 if (!_randomizeMonstersEnabled.Value)
@@ -83,7 +89,7 @@ namespace eradev.monstersanctuary.RandomRandomizer
                     return;
                 }
 
-                if (!_isRandomizerMode)
+                if (!GameModeManager.Instance.RandomizerMode)
                 {
                     _log.LogDebug("MonsterRandomizer ignore: Not in Randomizer mode");
 
@@ -98,15 +104,20 @@ namespace eradev.monstersanctuary.RandomRandomizer
                 }
 
                 _log.LogDebug("Randomizing encounter...");
-                _log.LogDebug("    Previous:");
+                _log.LogDebug("    Before:");
                 foreach(var m in __instance.PredefinedMonsters.Monster)
                 {
                     _log.LogDebug($"    * {GameModeManager.Instance.GetReplacementMonster(m.GetComponent<Monster>()).Name}");
                 }
 
+                var blacklistedMonsters = _monstersBlacklist.Value.Split(',').Select(int.Parse).ToList();
+                var availableMonsters = GameController.Instance.MonsterJournalList
+                    .Where(x => !blacklistedMonsters.Contains(x.GetComponent<Monster>().ID))
+                    .ToList();
+
                 for (var i = 0; i < 3; i++)
                 {
-                    __instance.PredefinedMonsters.Monster[i] = GameController.Instance.MonsterJournalList[Random.Range(4, 110)];
+                    __instance.PredefinedMonsters.Monster[i] = availableMonsters[Random.Range(0, availableMonsters.Count)];
                 }
 
                 _log.LogDebug("    After:");
@@ -120,6 +131,7 @@ namespace eradev.monstersanctuary.RandomRandomizer
         [HarmonyPatch(typeof(Chest), "OpenChest")]
         private class ChestOpenChestPatch
         {
+            // ReSharper disable once InconsistentNaming
             private static void Prefix(ref Chest __instance)
             {
                 if (!_randomizeChestsEnabled.Value)
@@ -129,7 +141,7 @@ namespace eradev.monstersanctuary.RandomRandomizer
                     return;
                 }
 
-                if (!_isRandomizerMode)
+                if (!GameModeManager.Instance.RandomizerMode)
                 {
                     _log.LogDebug("ChestRandomizer ignore: Not in Randomizer mode");
 
@@ -143,26 +155,45 @@ namespace eradev.monstersanctuary.RandomRandomizer
                     return;
                 }
 
-                if (__instance.name.Contains("SpecialChest"))
+                if (__instance.Item != null && __instance.Item.GetComponent<KeyItem>() != null)
                 {
-                    _log.LogDebug("ChestRandomize ignore: Special chest");
+                    _log.LogDebug("ChestRandomize ignore: Key chest");
+
+                    return;
+                }
+
+                if (__instance.Item != null && __instance.Item.GetComponent<UniqueItem>() != null)
+                {
+                    _log.LogDebug("ChestRandomize ignore: Unique item chest");
+
+                    return;
+                }
+
+                if (__instance.ID == 11)
+                {
+                    _log.LogDebug("ChestRandomize ignore: Raw Hide chest (Quest)");
 
                     return;
                 }
 
                 if (_possibleItemsList == null)
                 {
+                    var blacklistedItems = _itemsBlacklist.Value.Split(',').Select(int.Parse).ToList();
                     _possibleItemsList = GameController.Instance.WorldData.Referenceables
                         .Where(x => x?.gameObject?.GetComponent<BaseItem>() != null) // All the items
                         .Select(x => x.gameObject)
-                        .Where(x => x.GetComponent<KeyItem>() == null && x.GetComponent<UniqueItem>() == null) // Remove key and unique items
-                        .Where(x => x.GetComponent<Egg>() == null) // Remove Eggs
-                        .Where(x => x.GetComponent<Catalyst>() == null) // Remove Catalysts
+                        .Where(x => !blacklistedItems.Contains(x.GetComponent<BaseItem>().ID) &&  // Remove blacklisted items
+                                    x.GetComponent<KeyItem>() == null && // Remove Keys
+                                    x.GetComponent<UniqueItem>() == null && // Remove Unique items (ie. Costumes)
+                                    x.GetComponent<Egg>() == null && // Remove Eggs
+                                    x.GetComponent<Catalyst>() == null) // Remove Catalysts
                         .ToList();
                 }
 
                 _log.LogDebug("Randomizing chest content...");
-                _log.LogDebug($"    Previous: {(__instance.Gold > 0 ? $"{__instance.Gold} Gold" : __instance.Item.GetComponent<BaseItem>().Name)}");
+                _log.LogDebug($"    Chest ID: {__instance.ID}");
+                _log.LogDebug($"    Chest name: {__instance.name}");
+                _log.LogDebug($"    Before: {(__instance.Gold > 0 ? $"{__instance.Gold} Gold" : __instance.Item?.GetComponent<BaseItem>().GetName())}");
 
                 if (Rand.NextDouble() < _goldChance.Value)
                 {
@@ -173,29 +204,10 @@ namespace eradev.monstersanctuary.RandomRandomizer
                 {
                     __instance.Gold = 0;
                     __instance.Item = _possibleItemsList[Rand.Next(0, _possibleItemsList.Count)];
+                    __instance.Quantity = __instance.Item.GetComponent<Equipment>() != null ? 1 : Rand.Next(1, 4);
                 }
 
-                _log.LogDebug($"    After: {(__instance.Gold > 0 ? $"{__instance.Gold} Gold" : __instance.Item?.GetComponent<BaseItem>().Name)}");
-            }
-        }
-
-        [HarmonyPatch(typeof(GameModeManager), "LoadGame")]
-        private class GameModeManagerLoadGamePatch
-        {
-            // ReSharper disable once InconsistentNaming
-            private static void Postfix(ref GameModeManager __instance)
-            {
-                _isRandomizerMode = __instance.RandomizerMode;
-            }
-        }
-
-        [HarmonyPatch(typeof(GameModeManager), "SetupGame")]
-        private class GameModeManagerSetupGamePatch
-        {
-            // ReSharper disable once InconsistentNaming
-            private static void Postfix(ref GameModeManager __instance)
-            {
-                _isRandomizerMode = __instance.RandomizerMode;
+                _log.LogDebug($"    After: {(__instance.Gold > 0 ? $"{__instance.Gold} Gold" : __instance.Item?.GetComponent<BaseItem>().GetName())}");
             }
         }
     }
